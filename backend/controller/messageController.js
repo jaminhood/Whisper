f64d8fadb5cf1bac9365d4a57ce8c7b77b2031cb
Malachi <ukomalachi@gmail.com>
2023-11-20 02:36:14 +0100

    configure .env to use cloud database and added status to response.

--- backend/controller/messageController.js
+++ backend/controller/messageController.js
@@ -1,6 +1,7 @@
 const asyncWrapper = require('../lib/asyncWrapper.js')
 const Message = require('../model/messageModel.js')
 const mock_data = require('../mock_data.json')
+const BadRequestError = require('../errors/badRequest.js')
 
 const getMessages = asyncWrapper(async (req, res) => {
 
@@ -23,7 +24,7 @@ const getMessages = asyncWrapper(async (req, res) => {
 
     const SKIP = (page - 1) * LIMIT
     messages = await messages.skip(SKIP).limit(LIMIT)
-    res.json({ messages, hasNextPage: totalDocuments > (SKIP + LIMIT), hasPrevPage: page > 1, count: messages.length })
+    res.status(200).json({ messages, hasNextPage: totalDocuments > (SKIP + LIMIT), hasPrevPage: page > 1, count: messages.length })
 
 })
 
@@ -32,14 +33,14 @@ const postMessage = asyncWrapper(async (req, res) => {
     const { message } = req.body
 
     if (!message) {
-        throw new Error('message field is required.')
+        throw new BadRequestError('message field is required.')
     }
 
     //add new message to database.
     let msg = new Message({ message })
     msg = await msg.save()
 
-    res.json(msg)
+    res.status(200).json(msg)
 
 })
 
--- backend/middlewares/errorHandler.js
+++ backend/middlewares/errorHandler.js
@@ -1,7 +1,7 @@
 
 const errorHandler = (err, req, res, next) => {
     const reason = err.message
-    res.json({ msg: reason })
+    res.status(err.errorCode || 500).json({ msg: reason })
 }
 
 module.exports = { errorHandler }
